d4bea8698a6dce5b05898555e60f061709208ced
bavarianstance <bavarianstance@gmail.com>
2019-04-02 16:45:57 -0700

    fixed porting issue finally

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ const express = require("express");
 // init express and assign to variable 
 const app = express();
 // def ports
-const PORT = process.envPORT || 3000;
+const PORT = process.env.PORT || 3000;
 const HOST = "0.0.0.0";
 // init express data parsing handling
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, '/app/public/')));
 
 // def routing for app
 require("./app/routing/apiRoutes")(app);

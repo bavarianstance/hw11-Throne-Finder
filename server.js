9b76d08a3848a0c42cb9229f5702b87d42eaf7ab
bavarianstance <bavarianstance@gmail.com>
2019-04-02 09:59:13 -0700

    updated styling

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ let PORT = process.envPORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+// app.use(express.static('imgs'));
+// app.use(express.static('public'));
+
 
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);

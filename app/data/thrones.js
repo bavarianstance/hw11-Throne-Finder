edcf2058a4a870c7b8981f7131967e5824c26b48
bavarianstance <bavarianstance@gmail.com>
2019-04-01 18:06:22 -0700

    data entered and html started

---
+++ app/data/thrones.js
@@ -0,0 +1,220 @@
+let thrones = [
+
+{
+	"name": "Ned Stark",
+	"pic": "https://www.hbo.com/content/dam/hbodata/series/game-of-thrones/character/s5/eddard-ned-stark-512x512.jpg/_jcr_content/renditions/cq5dam.web.260.260.jpeg",
+	"scores": [
+	"4",
+	"4",
+	"3",
+	"5",
+	"6",
+	"6",
+	"4",
+	"4",
+	"5",
+	"4",
+	"4",
+	"4",
+	"4",
+	"5",
+	"3"
+	]
+},
+
+{
+	"name": "Tyrion Lannister",
+	"pic": "https://www.hbo.com/content/dam/hbodata/series/game-of-thrones/character/s5/tyrion-lannister-512x512.jpg/_jcr_content/renditions/cq5dam.web.260.260.jpeg",
+	"scores": [
+	"2",
+	"1",
+	"2",
+	"2",
+	"4",
+	"5",
+	"3",
+	"2",
+	"3",
+	"1",
+	"2",
+	"3",
+	"4",
+	"1",
+	"2"
+	]
+},
+
+{
+	"name": "Cersei Lannister",
+	"pic": "https://www.hbo.com/content/dam/hbodata/series/game-of-thrones/character/s5/cersei-lannister-512x512.jpg/_jcr_content/renditions/cq5dam.web.260.260.jpeg",
+	"scores": [
+	"2",
+	"1",
+	"3",
+	"4",
+	"5",
+	"5",
+	"3",
+	"2",
+	"4",
+	"3",
+	"2",
+	"3",
+	"1",
+	"4",
+	"1"
+},
+
+{
+	"name": "Jaime Lannister",
+	"pic": "https://www.hbo.com/content/dam/hbodata/series/game-of-thrones/character/s5/jamie-lannister-512x512.jpg/_jcr_content/renditions/cq5dam.web.260.260.jpeg",
+	"scores": [
+	"3",
+	"3",
+	"1",
+	"3",
+	"2",
+	"4",
+	"3",
+	"3",
+	"4",
+	"1",
+	"2",
+	"2",
+	"3",
+	"1",
+	"2"
+	]
+}.
+
+{
+	"name": "Daenerys Targaryen",
+	"pic": "https://www.hbo.com/content/dam/hbodata/series/game-of-thrones/character/s5/daenarys-512x512.jpg/_jcr_content/renditions/cq5dam.web.260.260.jpeg",
+	"scores": [
+	"1",
+	"3",
+	"2",
+	"2",
+	"5",
+	"5",
+	"2",
+	"1",
+	"4",
+	"2",
+	"2",
+	"3",
+	"1",
+	"4",
+	"3"
+	]
+},
+{
+	"name": "Jon Snow",
+	"pic": "https://www.hbo.com/content/dam/hbodata/series/game-of-thrones/character/s5/john-snow-512x512.jpg/_jcr_content/renditions/cq5dam.web.260.260.jpeg",
+	"scores": [
+	"1",
+	"2",
+	"3",
+	"4",
+	"6",
+	"5",
+	"2",
+	"4",
+	"3",
+	"3",
+	"2",
+	"1",
+	"3",
+	"4",
+	"2"
+	]
+},
+{
+	"name": "Sansa Stark",
+	"pic": "https://www.hbo.com/content/dam/hbodata/series/game-of-thrones/character/s5/sansa-stark-512x512.jpg/_jcr_content/renditions/cq5dam.web.260.260.jpeg",
+	"scores": [
+	"3",
+	"2",
+	"1",
+	"4",
+	"3",
+	"3",
+	"2",
+	"3",
+	"5",
+	"1",
+	"2",
+	"1",
+	"2",
+	"4",
+	"1"
+	]
+},
+{
+	"name": "Joffrey Baratheon",
+	"pic": "https://www.hbo.com/content/dam/hbodata/series/game-of-thrones/character/joffrey-baratheon-512.jpg/_jcr_content/renditions/cq5dam.web.260.260.jpeg",
+	"scores": [
+	"1",
+	"2",
+	"1",
+	"2",
+	"3",
+	"3",
+	"1",
+	"3",
+	"2",
+	"1",
+	"1",
+	"1",
+	"2",
+	"2",
+	"1"
+	]
+},
+{
+	"name": "Petyr Baelish",
+	"pic": "https://www.hbo.com/content/dam/hbodata/series/game-of-thrones/character/s5/petyr-baelish-512x512.jpg/_jcr_content/renditions/cq5dam.web.260.260.jpeg",
+	"scores": [
+	"2",
+	"3",
+	"1",
+	"2",
+	"3",
+	"3",
+	"2",
+	"1",
+	"4",
+	"3",
+	"2",
+	"3",
+	"4",
+	"4",
+	"2"
+	]
+},
+
+{
+	"name": "Khol Drogo",
+	"pic": "https://www.hbo.com/content/dam/hbodata/series/game-of-thrones/character/s5/khal-drogo-512x512.jpg/_jcr_content/renditions/cq5dam.web.260.260.jpeg",
+	"scores": [
+	"2",
+	"1",
+	"2",
+	"3",
+	"4",
+	"5",
+	"2",
+	"3",
+	"4",
+	"3",
+	"3",
+	"2",
+	"1",
+	"4",
+	"2"
+	]
+  }
+
+];
+
+module.exports = thrones;
